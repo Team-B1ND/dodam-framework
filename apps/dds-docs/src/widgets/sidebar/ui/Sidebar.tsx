f106078220fb7482dc2f8289a0e72f4fe42fe7db
김태우 <taewoo080401@gmail.com>
2026-01-10 12:54:24 +0900

    fix: 사이드바 내부 요소 패딩 수정

--- apps/dds-docs/src/widgets/sidebar/ui/Sidebar.tsx
+++ apps/dds-docs/src/widgets/sidebar/ui/Sidebar.tsx
@@ -7,13 +7,13 @@ const Sidebar = () => {
   return (
     <aside className="w-50 h-full fixed pt-16 overflow-y-scroll">
       <h2 className="text-text-placeholder text-sm mb-4">Indexes</h2>
-      <nav className="flex flex-col border-l border-border-normal pl-2">
+      <nav className="flex flex-col border-l border-border-normal pl-2 pr-4">
         {ROUTES.map((route) => (
           <Route key={route.href} {...route} />
         ))}
       </nav>
       <h2 className="text-text-placeholder text-sm mt-16 mb-4">Components</h2>
-      <nav className="flex flex-col border-l border-border-normal pl-2">
+      <nav className="flex flex-col border-l border-border-normal pl-2 pr-4">
         {COMPONENT_ROUTES.map((route) => (
           <Route key={route.href} {...route} />
         ))}
