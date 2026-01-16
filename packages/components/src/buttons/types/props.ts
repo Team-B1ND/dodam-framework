94e76addfd1f0c32bf8f9f19ddb8031e62585b6d
Cartridge0923 <pjmin0923@gmail.com>
2026-01-16 16:51:38 +0900

    feat: button props types

---
+++ packages/components/src/buttons/types/props.ts
@@ -0,0 +1,10 @@
+import { CSSObject } from "@emotion/styled";
+import { ComponentProps } from "react";
+import { ButtonSizes, FilledButtonRoles } from "../types/buttonSize";
+
+export interface FilledButtonProps extends ComponentProps<"button"> {
+  role: FilledButtonRoles;
+  size: ButtonSizes;
+  onClick: () => void;
+  buttonCustomStyle: CSSObject
+}
