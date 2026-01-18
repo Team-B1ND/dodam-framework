f920db778ae5e4631c6ff8784210752bc56b2bb0
Cartridge0923 <pjmin0923@gmail.com>
2026-01-18 16:02:19 +0900

    feat: style 분리

--- packages/components/src/avatar/ui/Avatar/index.tsx
+++ packages/components/src/avatar/ui/Avatar/index.tsx
@@ -1,29 +1,16 @@
 "use client"
 
 import { Person } from "@dds-web/iconography/mono"
-import styled from "@emotion/styled"
-import { AvatarProps } from "../../types/props"
 import { colors } from "@dds-web/colors"
+import { AvatarProps } from "../../types/props"
+import * as S from "./style"
 
 export const Avatar = ({
   size = 24
 }: AvatarProps) => {
   return (
-    <Container $size={size}>
+    <S.Container $size={size}>
       <Person size={size * 0.6} color={colors.fill.secondary}/>
-    </Container>
+    </S.Container>
   )
 }
-
-const Container = styled.div<{
-  $size: number;
-}>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: ${({ $size }) => `${$size}px`};
-  min-width: ${({ $size }) => `${$size}px`};
-  aspect-ratio: 1 / 1;
-  border-radius: 100%;
-  background-color: ${colors.fill.primary};
-`;
