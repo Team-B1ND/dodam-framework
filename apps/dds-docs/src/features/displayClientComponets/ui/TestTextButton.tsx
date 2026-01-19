edcf034c385e3ee954e44425aad3b31d4a3ba64c
Cartridge0923 <pjmin0923@gmail.com>
2026-01-19 16:54:32 +0900

    feat: text button docs

---
+++ apps/dds-docs/src/features/displayClientComponets/ui/TestTextButton.tsx
@@ -0,0 +1,9 @@
+import { TextButton, TextButtonProps } from '@dds-web/components'
+
+const TestTextButton = (props: TextButtonProps) => {
+  return (
+    <TextButton {...props}/>
+  )
+}
+
+export default TestTextButton
