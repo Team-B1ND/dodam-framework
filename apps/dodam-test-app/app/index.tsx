8526e169615073b861cf82379ac11a0d52267d65
김태우 <taewoo080401@gmail.com>
2026-02-20 15:35:14 +0900

    fix: 리액트 버전 일치

--- apps/dodam-test-app/app/index.tsx
+++ apps/dodam-test-app/app/index.tsx
@@ -1,6 +1,11 @@
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
 const App = () => {
   return (
-    <div>App</div>
+    <SafeAreaView>
+      <Text>Hello, Dodam Framework!</Text>
+    </SafeAreaView>
   )
 }
 
