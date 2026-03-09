db1f8d9864f18ab3af4f97685f4eb2a230945b26
김태우 <taewoo080401@gmail.com>
2026-03-09 20:29:19 +0900

    fix: 자잘한 긴급 수정

--- packages/dodam-design-system/src/components/buttons/types/buttonSize.ts
+++ packages/dodam-design-system/src/components/buttons/types/buttonSize.ts
@@ -1,3 +1,3 @@
-export type ButtonSizes = "large" | "mideum" | "small";
+export type ButtonSizes = "large" | "medium" | "small";
 export type FilledButtonRoles = "primary" | "negative" | "assistive";
 export type ButtonDisplay = "inline" | "fill";
